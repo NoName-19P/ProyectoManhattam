15dba1742a1fe37eb626fbca0d30f247ebd21455
Tardes <Tardes@GET-08>
2025-07-08 17:06:34 +0200

    cambio numeracion

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -3,7 +3,7 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 
-		System.out.println("HolaMundo ffffff");
+		System.out.println("abc");
 		 //Declarar variable
 		//Indico el tipo y el nombre de la variable(notacion camelCase)
 		int numero;
