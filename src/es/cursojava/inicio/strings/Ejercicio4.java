fc20df08b6cc6fddc1aef10354224b78d2abaf11
Luis Calderon Eclipse <Tardes@GET-08>
2025-07-09 20:47:25 +0200

    actual3

---
+++ src/es/cursojava/inicio/strings/Ejercicio4.java
@@ -0,0 +1,14 @@
+package es.cursojava.inicio.strings;
+
+public class Ejercicio4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * Escribe un programa que reciba un String con varias palabras separadas por espacios y determine cuál es la palabra más larga.
+
+		 */
+
+	}
+
+}
